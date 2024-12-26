3f0199516f8c368f49440c63438cb65e890a122e
FelipeSDS23 <felipe03san@gmail.com>
2024-12-26 15:07:16 -0300

    Enviando aulas de rotas, views e controllers

---
+++ app_super_gestao/routes/web.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+//controllers
+use App\Http\Controllers\PrincipalController;
+use App\Http\Controllers\SobreNosController;
+use App\Http\Controllers\ContatoController;
+
+Route::get('/', [PrincipalController::class, 'principal']);
+
+Route::get('/sobre-nos', [SobreNosController::class, 'sobreNos']);
+
+Route::get('/contato', [ContatoController::class, 'contato']);
