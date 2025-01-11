2cc52aafd92268f33c7348164bc5a4ebe86ffb2b
FelipeSDS23 <felipe03san@gmail.com>
2025-01-11 15:29:23 -0300

    Enviando arquivos

---
+++ app_super_gestao/database/seeders/FornecedorSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Fornecedor;
+use Illuminate\Support\Facades\DB;
+
+class FornecedorSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //instanciando o objeto
+        $fornecedor = new Fornecedor();
+        $fornecedor->nome = 'Fornecedor 100';
+        $fornecedor->site = 'fornecedor100.com.br';
+        $fornecedor->uf = 'CE';
+        $fornecedor->email = 'contato@forncedor100.com.br';
+        $fornecedor->save();
+
+        //usando o método create (atenção para o atributo fillable da classe)
+        Fornecedor::create([
+            'nome' => 'Fornecedor 200',
+            'site' => 'fornecedor200.com.br',
+            'uf' => 'RS',
+            'email' => 'contato@forncedor200.com.br'
+        ]);
+
+        //insert
+        DB::table('fornecedores')->insert([
+            'nome' => 'Fornecedor 300',
+            'site' => 'fornecedor300.com.br',
+            'uf' => 'SP',
+            'email' => 'contato@forncedor300.com.br'
+        ]);
+    }
+}
