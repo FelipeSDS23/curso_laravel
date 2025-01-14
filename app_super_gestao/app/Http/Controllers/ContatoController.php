7fac47cc0b9b7e50898bdf8b4ffcddee1845564f
FelipeSDS23 <felipe03san@gmail.com>
2025-01-14 19:48:03 -0300

    Enviando arquivos

--- app_super_gestao/app/Http/Controllers/ContatoController.php
+++ app_super_gestao/app/Http/Controllers/ContatoController.php
@@ -9,41 +9,24 @@ class ContatoController extends Controller
 {
     public function contato(Request $request) {
 
-        // dd($request->all());
+        $motivo_contatos = [
+            '1' => 'Dúvida',
+            '2' => 'Elogio',
+            '3' => 'Reclamação',
+        ];
 
-        // $contato = new SiteContato();
-
-        /*
-        $contato->nome = $request->input('nome');
-        $contato->telefone = $request->input('telefone');
-        $contato->email = $request->input('email');
-        $contato->motivo_contato = $request->input('motivo_contato');
-        $contato->mensagem = $request->input('mensagem');
-        $contato->save();
-        */
-
-        /*
-        $contato->fill($request->all());
-        $contato->save();
-        */
-
-        /*
-        $contato = new SiteContato();
-        $contato->create($request->all());
-        */
-
-        return view('site.contato', ['titulo' => 'Contato']);
+        return view('site.contato', ['titulo' => 'Contato', 'motivo_contatos' => $motivo_contatos]);
     }
 
     public function salvar(Request $request) {
 
         //realizar a validação dos 
         $request->validate([
-            'nome' => 'required',
+            'nome' => 'required|min:3|max:40',
             'telefone' => 'required',
             'email' => 'required',
             'motivo_contato' => 'required',
-            'mensagem' => 'required'
+            'mensagem' => 'required|max:2000'
         ]);
         // SiteContato::create($request->all());
     }
