29fec54549440f8cb0ffecda93707e0a78043a1c
FelipeSDS23 <felipe03san@gmail.com>
2025-02-24 12:18:18 -0300

    Enviando aulas

--- app_locadora_carros/app/Http/Controllers/MarcaController.php
+++ app_locadora_carros/app/Http/Controllers/MarcaController.php
@@ -18,14 +18,6 @@ class MarcaController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     */
-    // public function create()
-    // {
-    //     //
-    // }
-
-    /**
      * Store a newly created resource in storage.
      */
     public function store(Request $request)
@@ -41,14 +33,6 @@ class MarcaController extends Controller
     {
         return $marca;
     }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    // public function edit(Marca $marca)
-    // {
-    //     //
-    // }
 
     /**
      * Update the specified resource in storage.
