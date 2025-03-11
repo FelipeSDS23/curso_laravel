038b2e9274b685c652aca5977123805962dfbf13
FelipeSDS23 <felipe03san@gmail.com>
2025-03-11 11:08:24 -0300

    Enviando aulas

--- app_locadora_carros/routes/api.php
+++ app_locadora_carros/routes/api.php
@@ -21,9 +21,10 @@ Route::prefix('v1')->middleware(['auth:api'])->group(function () {
     Route::apiResource('locacao', LocacaoController::class);
     Route::apiResource('marca', MarcaController::class);
     Route::apiResource('modelo', ModeloController::class);
+
+    Route::post('me', [AuthController::class, 'me']);
+    Route::post('refresh', [AuthController::class, 'refresh']);
+    Route::post('logout', [AuthController::class, 'logout']);
 });
 
 Route::post('login', [AuthController::class, 'login']);
-Route::post('logout', [AuthController::class, 'logout']);
-Route::post('refresh', [AuthController::class, 'refresh']);
-Route::post('me', [AuthController::class, 'me']);
