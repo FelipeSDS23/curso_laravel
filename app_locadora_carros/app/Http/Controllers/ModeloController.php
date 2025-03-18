3cc250e1317f26059bf6f3796682f162e5326d76
FelipeSDS23 <felipe03san@gmail.com>
2025-03-18 08:51:54 -0300

    Enviando aulas

--- app_locadora_carros/app/Http/Controllers/ModeloController.php
+++ app_locadora_carros/app/Http/Controllers/ModeloController.php
@@ -52,7 +52,6 @@ class ModeloController extends Controller
 
     /**
      * Store a newly created resource in storage.
-     *
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
